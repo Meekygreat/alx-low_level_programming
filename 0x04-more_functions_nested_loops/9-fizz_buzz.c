177a564c729fabbe76a0c422226b66d5d862353f
meeky <chikameekky@gmail.com>
2022-09-15 18:37:41 -0700

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,66 +1,41 @@
-main.h.txt
-Who has access
-A
-System properties
-Type
-Text
-Size
-456 bytes
-Storage used
-456 bytes
-Location
-More functions, more nested loops
-	Owner
-	Antony Bahati
-	Modified
-	11:49 AM by Antony Bahati
-	Opened
-	9:48 PM by me
-	Created
-	11:51 AM
-	No description
-	Viewers can download
-	#include <stdio.h>
+#include "main.h"
+#include <stdio.h>
 
-	/**
-	 *main - program that prints either number
-	 *or fizz or buzz or fizzBuzz
-	 *
-	 *
-	 *Return: returns 0
-	 */
-
-	int main(void)
+/**
+ * main - Fizz for multiples of 3 and Buzz for multiples of 5 FizzBuzz for both
+ *
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
-	int num = 1;
+	int x = 1;
 
-	while (num++ < 100)
+	while (x < 101)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (x % 3 == 0 && x % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("%s", "FizzBuzz");
 		}
-		else if ((num % 3) == 0)
+		else if (x % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("%s", "Fizz");
 		}
-		else if ((num % 5) == 0)
+		else if (x % 5 == 0)
 		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz");
-			}
+			printf("%s", "Buzz");
 		}
 		else
 		{
-			printf("%d ", num);
+			printf("%d", x);
 		}
+
+		if (x != 100)
+		{
+			printf(" ");
+		}
+		x++;
 	}
 	printf("\n");
-
 	return (0);
 }
