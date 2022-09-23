712b185b9564b5a1ce328aaf8da6e51f3cb822f6
meeky <chikameekky@gmail.com>
2022-09-22 17:33:21 -0700

    changing lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,13 @@
 
 char *string_toupper(char *str)
 {
-	int x = 0;
+	int i = 0;
 
-	while (str[x] != '\0')
+	while (str[i] != '\0')
 	{
-		if (str[x] >= 'a' && str[i] <= 'z')
-			str[x] -= 32;
-		x++;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
 	}
 	return (str);
 }
