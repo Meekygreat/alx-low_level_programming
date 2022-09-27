f71a5e735d814f668a24de4e3eeb2affc8bd4df8
meeky <chikameekky@gmail.com>
2022-09-26 17:42:40 -0700

    Write a function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,21 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
-/**
-
- * void prototypes()
- * int prototypes()
- */
-
-int _putchar(char);
-char *_memset(char *s, char b, unisgned int n);
-char *_memcpy(char *dest, char *src, unisigned int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unisigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-char *_strstr(char $haystack, char *needle);
+char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void print_diagmus(int *a, int size);
-void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
 
-#endif /*MAIN_H*/
+#endif
