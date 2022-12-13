0288352018b5615377af917a445e8109f35a5b2d
meeky <chikameekky@gmail.com>
2022-12-12 20:34:12 -0800

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,12 @@
+/*
+ * File: 0-print_dlistint.c
+ * Auth: Nicolas Peter Wanjau
+ */
+
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
+ * print_dlistint - Prints all the elements of a dlistint_t list.
  * @h: The head of the dlistint_t list.
  *
  * Return: The number of nodes in the list.
